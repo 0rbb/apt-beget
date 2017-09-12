4bd7e79daa5af26e8dd2b6db9ab7f2a261c37f17
ford153focus <ford153focus@gmail.com>
2017-09-12 03:51:38 +0300

    update ror installer

--- 20ror.sh
+++ 20ror.sh
@@ -1,12 +1,21 @@
-function install_ror {
-    #curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-    #curl -sSL https://get.rvm.io | bash -s stable --ruby --gems=rails,rb-readline,execjs,therubyracer
+:<<=
+==Ruby On Rails
+=
+function install_default {
     check_ds
-    chruby ruby-2.2.2
-    echo 2.2.2 > $HOME/.ruby-version
+    echo_y "Installing Ruby On Rails..."
+
+    #prepare folders
+    echo_y "Preparing folders..."
+    prepare_folders
+
+    #install
+    echo_y "Installing..."
+    chruby ruby-2.3
+    echo '2.3' > $HOME/.ruby-version
     cd $HOME
-    ls -la | grep -v beget_install | awk '{print $9}'| xargs rm -rf # delete all exept myself
     gem install rails --no-rdoc --no-ri
+    gem install rb-readline --no-rdoc --no-ri
     rails new .
     ln -sf public public_html
     echo "gem 'rb-readline'
@@ -14,14 +23,19 @@ function install_ror {
     gem 'therubyracer'
     gem 'sqlite3'" >> $HOME/Gemfile
     bundle install
+
+    echo_y "Setting up..."
     echo "RailsEnv development
-        PassengerUploadBufferDir `pwd`/tmp
-        PassengerRuby /opt/rubies/ruby-2.2.2/bin/ruby
-        PassengerAppRoot `pwd`
-        SetEnv GEM_HOME $HOME/.gem/ruby/2.2.2/:/opt/rubies/ruby-2.2.2/lib/ruby/gems/2.2.0/" >> $HOME/.htaccess
-    echo "ENV['GEM_HOME'] = '$HOME/.gem/ruby/2.2.2/'
-ENV['GEM_PATH'] = '$HOME/.gem/ruby/2.2.2/'
+PassengerUploadBufferDir `pwd`/tmp
+PassengerRuby /opt/rubies/ruby-2.3/bin/ruby
+PassengerAppRoot `pwd`
+SetEnv GEM_HOME $HOME/.gem/ruby/2.3.1/:/opt/rubies/ruby-2.3/lib/ruby/gems/2.3.0/" >> $HOME/.htaccess
+    echo "ENV['GEM_HOME'] = '$HOME/.gem/ruby/2.3.1/'
+ENV['GEM_PATH'] = '$HOME/.gem/ruby/2.3.1/'
 require 'bundler/setup'" >> $HOME/config/setup_load_paths.rb
     mkdir $HOME/tmp
     touch $HOME/tmp/restart.txt
+
+    #finish
+    echo_g "Ruby On Rails installed"
 }

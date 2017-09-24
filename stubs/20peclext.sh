0ea7ceb36ce618b1d2d4c542380d485b712becd4
Joe Ford <ford153focus@gmail.com>
2017-09-24 07:35:05 +0300

    ask php version and change install path

--- stubs/20peclext.sh
+++ stubs/20peclext.sh
@@ -5,6 +5,13 @@
 function install_peclextension {
     check_ds
     echo_y "Installing peclextension..."
+    
+    echo_y "Enter PHP version (default is 5.6)"
+    read var
+    if [[ ! $var ]]
+    then
+        var='5.6'
+    fi
 
     #prepare folders
     echo_y "Preparing folders..."
@@ -33,17 +40,17 @@ function install_peclextension {
     echo_y "Compilating..."
     PATH=$PATH:/usr/local/php-cgi/5.6/bin/
 
-    mkdir -p $HOME/.local/lib/php/56/
+    mkdir -p $HOME/.local/php/cgi/$var/lib/php/
     
     cd $HOME/.beget/tmp/peclextension
     phpize
-    ./configure --prefix=$HOME/.local/lib/php/56/
+    ./configure --prefix=$HOME/.local/php/cgi/$var/lib/php/
     make
     make install #ignore fail
     
-    cp $HOME/.beget/tmp/peclextension/modules/peclextension.so $HOME/.local/lib/php/56/
+    cp $HOME/.beget/tmp/peclextension/modules/peclextension.so $HOME/.local/php/cgi/$var/lib/php/
 
-    if [ ! -f $HOME/.local/lib/php/56/peclextension.so ]
+    if [ ! -f $HOME/.local/php/cgi/$var/lib/php/peclextension.so ]
     then
         echo_r 'seems like compilation is failed'
         exit 1
@@ -57,10 +64,9 @@ function install_peclextension {
         cp /etc/php/cgi/5.6/php.ini $HOME/public_html/cgi-bin/php.ini
     fi
     sed -i 's/; EOF//g' $HOME/public_html/cgi-bin/php.ini
-    printf "\n\n[PHP]\nextension = $HOME/.local/lib/php/56/peclextension.so" >> $HOME/public_html/cgi-bin/php.ini
+    printf "\n\n[PHP]\nextension = $HOME/.local/php/cgi/$var/lib/php/peclextension.so" >> $HOME/public_html/cgi-bin/php.ini
 
-    cd $HOME/public_html
-    echo '<?php phpinfo();' > x.php
+    echo '<?php phpinfo();' > $HOME/public_html/x.php
 
     #finish
     echo_g "peclextension installed"

eb267d47f53dc2d418c2f2f1c241bb76ee5d6a8b
Joe Ford <ford153focus@gmail.com>
2017-10-05 11:46:27 +0300

    update python version

--- installers/python3.sh
+++ installers/python3.sh
@@ -8,8 +8,8 @@ function install_python3 {
 
     #Download
     echo_y 'Downloading...'
-    curl -Lk https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz > $HOME/.beget/tmp/Python-3.6.2.tgz
-    if [ ! -f "$HOME/.beget/tmp/Python-3.6.2.tgz" ]
+    curl -Lk https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tgz > $HOME/.beget/tmp/Python-3.6.3.tgz
+    if [ ! -f "$HOME/.beget/tmp/Python-3.6.3.tgz" ]
     then
         echo_r 'download is failed'
         exit 1
@@ -18,8 +18,8 @@ function install_python3 {
     #Unpacking
     cd $HOME/.beget/tmp
     echo_y 'Unpacking...'
-    tar xvf Python-3.6.2.tgz
-    if [ ! -d "$HOME/.beget/tmp/Python-3.6.2" ]
+    tar xvf Python-3.6.3.tgz
+    if [ ! -d "$HOME/.beget/tmp/Python-3.6.3" ]
     then
         echo_r "Seems like unpacking is failed"
         exit 1
@@ -27,9 +27,9 @@ function install_python3 {
 
     #Compiling
     echo_y "Compiling..."
-    cd Python-3.6.2
+    cd Python-3.6.3
     ./configure --prefix $HOME/.local
-    make -j $(expr $(nproc) / 4)
+    make -j $(expr $(nproc) / 21)
     make install
     if [ ! -f "$HOME/.local/bin/pip3" ]
     then
